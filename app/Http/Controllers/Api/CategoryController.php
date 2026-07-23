64207428160825fa544951cb3d107b9d936448bd
Ömer Eroğlu <omereroglu1923@gmail.com>
2026-07-23 14:06:50 +0300

    Category API endpoint with Sanctum installed

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        // Eloquent collection döndürüyoruz, Laravel otomatik JSON'a çeviriyor
+        return Category::all();
+    }
+}
